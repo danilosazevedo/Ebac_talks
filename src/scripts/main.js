d8bfd3c3008dffee091186c3e4e009d7d799bef5
danilosazevedo <danilo-sa@live.com>
2023-01-24 15:14:11 -0300

    criando a data do evento

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
-console.log("Ola Mundo")
+AOS.init();
+
+const dataDoEvento = new Date("Dec 12, 2023 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function(){
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diasAteOAteOEvento = Math.floor(distanciaAteOEvento / (1000 * 60 * 60 * 24));
+    const horasAteoEvento = Math.floor((distanciaAteOEvento % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % (1000 * 60*  60)) / (1000 * 60));
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % (1000 * 60)) / 1000);
+
+    console.log(diasAteOAteOEvento);
+    console.log(horasAteoEvento);
+    console.log(minutosAteOEvento);
+    console.log(segundosAteOEvento);
+
+    document.getElementById('contador').innerHTML = `${diasAteOAteOEvento}d ${horasAteoEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`
+
+    if(distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Aguarde a Nova Data'
+    }
+},1000);
